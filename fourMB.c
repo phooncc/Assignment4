e96b38994c99050bd3ccfe7049509a0e4748de00
phooncc <phooncc@live.com.sg>
2017-04-05 10:01:38 +0800

    Extending onebyte to fourMB
    Changed the kmalloc value.
    Changed the read copy*() value.
    Changed to write copy*() value.

--- fourMB.c
+++ fourMB.c
@@ -11,37 +11,37 @@
 #define MAJOR_NUMBER 61
 
 /* forward declaration */
-int fourMB_open(struct inode *inode, struct file *filep);
-int fourMB_release(struct inode *inode, struct file *filep);
-ssize_t fourMB_read(struct file *filep, char *buf, size_t
+int onebyte_open(struct inode *inode, struct file *filep);
+int onebyte_release(struct inode *inode, struct file *filep);
+ssize_t onebyte_read(struct file *filep, char *buf, size_t
 count, loff_t *f_pos);
-ssize_t fourMB_write(struct file *filep, const char *buf,
+ssize_t onebyte_write(struct file *filep, const char *buf,
 size_t count, loff_t *f_pos);
-static void fourMB_exit(void);
+static void onebyte_exit(void);
 
 /* definition of file_operation structure */
-struct file_operations fourMB_fops = {
-	read:fourMB_read,
-	write:fourMB_write,
-	open:fourMB_open,
-	release: fourMB_release
+struct file_operations onebyte_fops = {
+	read:onebyte_read,
+	write:onebyte_write,
+	open:onebyte_open,
+	release: onebyte_release
 };
 
-char *fourMB_data = NULL;
+char *onebyte_data = NULL;
 
-int fourMB_open(struct inode *inode, struct file *filep)
+int onebyte_open(struct inode *inode, struct file *filep)
 {
 	return 0; // always successful
 }
-int fourMB_release(struct inode *inode, struct file *filep)
+int onebyte_release(struct inode *inode, struct file *filep)
 {
 	return 0; // always successful
 }
-ssize_t fourMB_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
+ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
 /*please complete the function on your own*/
 	if(*f_pos == 0){
-		copy_to_user(buf,fourMB_data,1);
+		copy_to_user(buf,onebyte_data,4e6);
 		*f_pos = *f_pos + 1;
 		return 1;	
 	}
@@ -50,11 +50,11 @@ ssize_t fourMB_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 	}
 	return 0;
 }
-ssize_t fourMB_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
+ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
 /*please complete the function on your own*/
 	if(*f_pos == 0){
-		copy_from_user(fourMB_data, buf, 1);
+		copy_from_user(onebyte_data, buf, 4e6);
 		*f_pos = *f_pos + 1;
 		return 1;
 	}
@@ -62,42 +62,42 @@ ssize_t fourMB_write(struct file *filep, const char *buf, size_t count, loff_t *
 		return -ENOSPC;
 	}
 }
-static int fourMB_init(void)
+static int onebyte_init(void)
 {
 	int result;
 	// register the device
-	result = register_chrdev(MAJOR_NUMBER, "fourMB", &fourMB_fops);
+	result = register_chrdev(MAJOR_NUMBER, "onebyte", &onebyte_fops);
 	if (result < 0) {
 		return result;
 	}
 	// allocate one byte of memory for storage
 	// kmalloc is just like malloc, the second parameter is// the type of memory to be allocated.
 	// To release the memory allocated by kmalloc, use kfree.
-	fourMB_data = kmalloc(1024*4000, GFP_KERNEL); //1kB * 4000 = 4MB - allocation 4MB memory
-	if (!fourMB_data) {
-		fourMB_exit();
+	onebyte_data = kmalloc(4e6, GFP_KERNEL);//4e6 = 4MB
+	if (!onebyte_data) {
+		onebyte_exit();
 	// cannot allocate memory
 	// return no memory error, negative signify a failure
 		return -ENOMEM;
 	}
 	// initialize the value to be X
-	*fourMB_data = 'X';
-	printk(KERN_ALERT "This is a fourMB device module\n");
+	*onebyte_data = 'X';
+	printk(KERN_ALERT "This is a onebyte device module\n");
 	return 0;
 }
-static void fourMB_exit(void)
+static void onebyte_exit(void)
 {
 	// if the pointer is pointing to something
-	if (fourMB_data) {
+	if (onebyte_data) {
 		// free the memory and assign the pointer to NULL
-		kfree(fourMB_data);
-		fourMB_data = NULL;
+		kfree(onebyte_data);
+		onebyte_data = NULL;
 	}
 	// unregister the device
-	unregister_chrdev(MAJOR_NUMBER, "fourMB");
+	unregister_chrdev(MAJOR_NUMBER, "onebyte");
 	printk(KERN_ALERT "Onebyte device module is unloaded\n");
 }
 MODULE_LICENSE("GPL");
-module_init(fourMB_init);
-module_exit(fourMB_exit);
+module_init(onebyte_init);
+module_exit(onebyte_exit);
 
