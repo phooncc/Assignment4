831aed2c553d46bd0a857c841aef14e1928e2aa4
phooncc <phooncc@live.com.sg>
2017-04-09 15:15:51 +0800

    Add files via upload

--- 4MB.c
+++ 4MB.c
@@ -28,6 +28,7 @@ struct file_operations onebyte_fops = {
 };
 
 char* onebyte_data = NULL;
+int overflowed_byte = 0;
 
 int onebyte_open(struct inode *inode, struct file *filep)
 {
@@ -41,14 +42,26 @@ ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
 	//if(*f_pos == 0){
 		//copy_to_user(buf, onebyte_data, 1);
-	printk("Reading");
-	printk("count: %d, *f_pos: %d",(count, *f_pos));
-	int len = count - *f_pos;//= size;
+	printk("---------------Reading------------");
+	printk("count: %d",(count));
+	printk("*f_pos: %d",*f_pos);
+	int len;
+	if(*f_pos + count - 1 <= size - 1)len = count;
+	else len = count - (*f_pos + count -1 - size + 1);
+	printk("len to be copy : %d", len);
 	if(*f_pos < size){
 		long dest_address = buf;
 		long source_address = onebyte_data + *f_pos;
 		copy_to_user(dest_address, source_address, len);//Copy byte by byte//copy_to_user(to,from,n)
 		*f_pos = *f_pos + len;
+		int k = 0, i =0;
+		while(1){
+			if(*(onebyte_data + i) == NULL)k++;
+			i++;
+			if(i == size)break;
+		}
+		printk("Number of NULL: %d",(k));
+		printk("Number of bytes written to buffer: %d", size - k);
 		return len;
 	}
 	else if(*f_pos >= size){
@@ -59,28 +72,54 @@ ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
 	//Here we need to recognise that if the device must read full 4MB, this function will execute again and again
-	//Say if 4MB is the kmalloc size and buffer size is 13kB, the number of times executing this function will be
+	//Say if 4MB is the kmalloc size and buffer size is 13kB, the number of times executing this functeion will be
 	//307.69. 307 times of buffer len and one last time with buffer len of 0.6923 of 13 kB.
-	//             |------------------|-----------------------------------------|
+	//Here we compare the using the actual (kmalloc size - *f_pos) and actual buffer length.
+	// First case: |------------------|-----------------------------------------|
 	//             ^(*f_pos)          ^(buf_size)                               ^(max_kmalloc)
-	//count will adjust itself automatically
-	printk("Writing");
-	printk("count: %d, *f_pos: %d",(count, *f_pos));
-	int len = count - *f_pos;
+	// Second case:|---------------------------------------------------------|--|-------------------|
+	printk("---------------Writing------------");
+	printk("count: %d",(count));
+	printk("*f_pos: %d",*f_pos);
+	//For example if the input contains 8 bytes -> count = 8//Another example, input is 1MB, count -> 131072 (max)
+	//Important thing is that *f_pos must not be more than fsize (allocated memory) or else will be writing to unauthorized region
+	//Therefore, I need to check if the current *f_pos + count will exceed fsize or not. Remember *f_pos start from 0, fsize does not!
+	//It will be easier to compare through positions instead of fsize
+	int len = count;
+	int maxfpos = size - 1;
+	int new_max_written_fos_pos = *f_pos + len - 1;
+	if(new_max_written_fos_pos <= maxfpos){
+		//do nth
+	}
+	else{ //I need to adjust the len//
+		len = len - (new_max_written_fos_pos - maxfpos);
+	}
 	if(*f_pos < size){
 		long source_address = buf;
 		long dest_address = onebyte_data + *f_pos;
-		copy_from_user(dest_address, source_address, len);//copy_from_user(to,from,n)//Use len-1 to exclude "wwerwre\r" -> '\r'
+		copy_from_user(dest_address, source_address, len);//copy_from_user(to,from,n)//len-1 to exclude "wwerwre\r" -> '\r'
 		*f_pos = *f_pos + len;
+		int k = 0, i =0;
+		while(1){
+			if(*(onebyte_data + i) == NULL)k++;
+			i++;
+			if(i == size)break;
+		}
+		printk("Number of NULL: %d",(k));
+		printk("Number of bytes written to file: %d", size - k);
 		return len;
 	}
-	else if(*f_pos >= size){
-		return -ENOSPC;
+	else if(*f_pos == size){
+		if(count == 0) return 0;
+		overflowed_byte += count;
+		printk("Number of bytes overflowed: %d",overflowed_byte);
+		return count;
 	}
 }
 static int onebyte_init(void)
 {
 	int result;
+	// register the device
 	result = register_chrdev(MAJOR_NUMBER, "onebyte", &onebyte_fops);
 	if (result < 0) {
 		return result;
@@ -91,11 +130,15 @@ static int onebyte_init(void)
 	kfree(onebyte_data);
 	onebyte_data = kmalloc(size, GFP_KERNEL);
 	printk("I got %zu bytes of memory",ksize(onebyte_data));
-	memset(onebyte_data, NULL, size);
+	int i = 0;
+	while(i<size){
+		*(onebyte_data + i) = NULL;
+		i++;
+	}
+	printk("Reset %d bytes of memory to NULL",i);
+	//memset(onebyte_data, NULL, size);
 	if (!onebyte_data) {
 		onebyte_exit();
-	// cannot allocate memory
-	// return no memory error, negative signify a failure
 		return -ENOMEM;
 	}
 	// initialize the value to be X
@@ -109,6 +152,7 @@ static void onebyte_exit(void)
 	if (onebyte_data) {
 		// free the memory and assign the pointer to NULL
 		kfree(onebyte_data);
+		//memset(onebyte_data, NULL, size);
 		onebyte_data = NULL;
 	}
 	// unregister the device
